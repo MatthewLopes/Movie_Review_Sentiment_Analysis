8f21caed0ec63e954f24bf60c04234fc17d87427
Jack Kovach <jackwkovach@gmail.com>
2022-11-16 21:01:46 -0500

    0.96 passing upload to new repo - JWK

---
+++ mymain.R
@@ -0,0 +1,53 @@
+#####################################
+# Load libraries
+# Load your vocabulary and training data
+#####################################
+library(text2vec)
+library(glmnet)
+library(pROC)
+library(slam)
+library(dplyr)
+
+myvocab <- scan(file = "myvocab.txt", what = character())
+train = read.table("train.tsv",
+                   stringsAsFactors = FALSE,
+                   header = TRUE)
+train$review = gsub('<.*?>', ' ', train$review)
+
+vectorizer = vocab_vectorizer(create_vocabulary(myvocab,
+                                                ngram = c(1L, 2L)))
+
+#####################################
+# Train a binary classification model
+#####################################
+it_train = itoken(train$review,
+                  preprocessor = tolower, 
+                  tokenizer = word_tokenizer)
+
+dtm_train = create_dtm(it_train, vectorizer)
+
+fit <- cv.glmnet(y = train$sentiment, x = dtm_train, family = 'binomial', alpha = 0)
+
+#####################################
+# Load test data, and 
+# Compute prediction
+#####################################
+test <- read.table("test.tsv", stringsAsFactors = FALSE,
+                   header = TRUE)
+
+it_test = itoken(test$review,
+                 preprocessor = tolower,
+                 tokenizer = word_tokenizer)
+
+dtm_test = create_dtm(it_test, vectorizer)
+
+output[,1] <- test$id
+output[,2] <- predict(fit, dtm_test, type="response", s = fit$lambda.1se)
+
+#####################################
+# Store your prediction for test data in a data frame
+# "output": col 1 is test$id
+#           col 2 is the predicted probs
+#####################################
+write.table(output, file = "mysubmission.txt", 
+            row.names = FALSE, sep='\t')
